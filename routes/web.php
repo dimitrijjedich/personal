c9d255eda0a7785dd211e39d61ab930ebe500508
dimitrijjedich <dimitrij@startup-werk.de>
2024-07-10 19:59:03 +0200

    add examples for named routes and their usage

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,20 @@ Route::get('/', function () {
 
 Route::get('about', function () {
     return '<h1>About Page</h1>';
-});
+})->name('about');
 
 Route::get('contact', function () {
     return '<h1>Contact Page</h1>';
-});
+})->name('contact');
 
 Route::get('contact/{id}', function ($id) {
     return $id;
+})->name('contact-named');
+
+Route::get('home', function () {
+    return '
+    <a href="'.route('about').'">About</a>
+    <a href="'.route('contact').'">Contact</a>
+    <a href="'.route('contact-named', ['id' => 123]).'">Contact Named</a>
+    ';
 });

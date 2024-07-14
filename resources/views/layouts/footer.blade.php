9dde42ada1e76f2ca2de747bbebf134540a9f41b
dimitrijjedich <dimitrij@startup-werk.de>
2024-07-14 15:39:52 +0200

    extract header and footer into seperated views

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="bg-body-tertiary text-center text-lg-start" style="position: fixed; width: 100%; bottom: 0">
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2020 Copyright:
+        <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+</footer>

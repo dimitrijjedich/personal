e3a43c25dae281bc24f48ea4d5c339788c326358
dimitrijjedich <dimitrij@startup-werk.de>
2024-07-16 16:07:26 +0200

    build basic login form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,5 +1,28 @@
 @extends('layouts.master')
 
 @section('content')
-    <h1>This is the Login Page</h1>
+    <div class="row mt-5 justify-content-center">
+        <div class="col-md-4">
+            <h2 class="mb-4">Login</h2>
+            <div class="card">
+                <div class="card-body">
+                    <form action="">
+                        <div class="mb-2">
+                            <label for="" class=""form-label">User Name</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="mb-2">
+                            <label for="" class=""form-label">User Email</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <div class="mb-2">
+                            <label for="" class=""form-label">Password</label>
+                            <input type="email" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection()

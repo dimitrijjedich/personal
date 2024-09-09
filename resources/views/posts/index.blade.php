d3de2e6612c188057267c94466daf05e5d236397
dimitrijjedich <dimitrij@startup-werk.de>
2024-09-09 17:56:59 +0200

    change width of last column

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -18,7 +18,7 @@
                         <th scope="col" style="width: 30%">Description</th>
                         <th scope="col" style="width: 10%">Category</th>
                         <th scope="col" style="width: 10%">Publish Date</th>
-                        <th scope="col" style="width: 10%">Action</th>
+                        <th scope="col" style="width: 20%">Action</th>
                     </tr>
                     </thead>
                     <tbody>

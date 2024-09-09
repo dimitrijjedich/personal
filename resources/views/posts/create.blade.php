3c2857921863feb85254a08cc61696c8bb4e9d0f
dimitrijjedich <dimitrij@startup-werk.de>
2024-09-09 20:34:50 +0200

    add route and csrf to create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,7 +14,8 @@
                 </div>
             </div>
             <div class="card-body">
-                <form action="" method="POST" enctype="multipart/form-data">
+                <form action="{{route('posts.store')}}" method="POST" enctype="multipart/form-data">
+                    @csrf
                     <div class="form-group">
                         <label for="" class="form-label">Image</label>
                         <input type="file" class="form-control" name="image">

abb0482a7f0ff6581aa2270b7da02b9bd0dda01d
dimitrijjedich <dimitrij@startup-werk.de>
2024-09-17 18:20:52 +0200

    refactor post-tags relation to category

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -13,8 +13,8 @@ class Post extends Model
 
     protected $guarded = ['id'];
 
-    public function tags(): belongsToMany
+    public function category(): BelongsTo
     {
-        return $this->belongsToMany(Tag::class);
+        return $this->belongsTo(Category::class);
     }
 }

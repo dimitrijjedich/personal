e50b369eea341664ed3cb702d17a4310ffb7616a
dimitrijjedich <dimitrij@startup-werk.de>
2024-09-21 12:04:46 +0200

    remove initial seeder content

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,13 +23,6 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        DB::table('posts')->insert([
-            'title' => "This is a test Data",
-            'description' => 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
-            'status' => 1,
-            'publish_date' => date('Y-m-d'),
-            'user_id' => 1,
-        ]);
         foreach ($users as $user) {
             Post::factory(rand(10,30))->create(['user_id' => $user->id]);
         }
